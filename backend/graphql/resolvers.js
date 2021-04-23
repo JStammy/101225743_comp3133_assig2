ec795f19f9697ea76d5fca312edaf12539238424
JStammy <48102286+JStammy@users.noreply.github.com>
2021-04-23 00:55:02 -0400

    updating and adding addBooking

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -13,6 +13,27 @@ module.exports = {
       })
     };
   },
+  
+         addBooking: async (parent, args) => {
+            console.log(args)
+            
+            const dateExpression = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
+            const isValidDate = dateExpression.test(String(args.booking_date, args.booking_start, args.booking_end))
+            
+            if(!isValidDate){
+                throw new Error("Please follow the proper format. MM/DD/YYYY.")
+            }
+
+
+            let newBooking = new Booking ({
+                hotel_id: args.hotel_id,
+                booking_date: args.booking_date,
+                booking_start: args.booking_start,
+                booking_end: args.booking_end,
+                user_id: args.user_id
+            });
+            return await newBooking.save()
+  
   addUser: async function ({ userInput }) {
     const user = new User({
       user_id: userInput.user_id,
