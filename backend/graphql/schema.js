761269066eea288591e410a282ed85645c8eab2c
JStammy <48102286+JStammy@users.noreply.github.com>
2021-04-23 00:53:07 -0400

    Update schema.js
    adding mutation and addBooking

--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -17,6 +17,29 @@ module.exports = buildSchema(`
         email: String!
         user_id: Int!
     }
+
+
+    type Mutation {
+    addHotel(
+        hotel_id: ID!
+        hotel_name: String!
+        street: String!
+        city: String!
+        postal_code: String!
+        price: Int!
+        email: String!
+        user_id: ID!
+        ): Hotel
+    
+    addBooking(
+        hotel_id: ID!
+        booking_date: String!
+        booking_start: String!
+        booking_end: String!
+        user_id: String!
+    ): Booking
+
+    
     type UserData {
         users: [User!]!
     }
