d91fff4eed0eca6e2b5ef31c929d595729d2fadf
JStammy <48102286+JStammy@users.noreply.github.com>
2021-04-23 01:01:37 -0400

    adding dateRegex to validate proper date format

--- backend/models/hotelBookings.js
+++ backend/models/hotelBookings.js
@@ -10,15 +10,30 @@ const BookingSchema = new mongoose.Schema({
   booking_date: {
     type: String,
     default: Date.now(),
-    required: [true, 'Please eneter booking date']
+    required: [true, 'Please eneter booking date'],
+    lowercase: true,
+            validate: function(value) {
+                var dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
+                return dateRegex.test(value)
+            }
   },
   booking_start: {
     type: String,
-    required: [true, 'Please enter the starting date']
+    required: [true, 'Please enter the starting date'],
+    lowercase: true,
+            validate: function(value) {
+                var dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
+                return dateRegex.test(value)
+            }
   },
   booking_end: {
     type: String,
-    required: [true, 'Please enter the ending date']
+    required: [true, 'Please enter the ending date'],
+    lowercase: true,
+            validate: function(value) {
+                var dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
+                return dateRegex.test(value)
+            }
   },
   user_id: {
     type: Number,
